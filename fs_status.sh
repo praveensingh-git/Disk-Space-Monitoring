0658117140b585f3a8c78df5c5222d76d4ceaff2
Praveen Singh <153358178+praveensingh-git@users.noreply.github.com>
2025-09-05 14:54:30 +0530

    for testing

--- fs_status.sh
+++ fs_status.sh
@@ -4,11 +4,9 @@
 # Author: NedStark (Kali Linux)
 # Description: Sends an email report of disk usage
 # =========================================
-: <<'END_COMMENT'
-
-
-                
-END_COMMENT
+#
+#
+#
 TO="praveensuryavanshi021@gmail.com"
 
 # Disk usage
